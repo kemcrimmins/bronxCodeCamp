5e4155d61f1784ad1570e63a31ef6ff23e804290
Kem Crimmins <kem.crimmins@gmail.com>
2017-02-18 11:03:00 -0500

    add comment to members database in array

--- displayMembers.js
+++ displayMembers.js
@@ -1,8 +1,10 @@
+/* Eventually the database will be imported as a module and , thus, will reside in a separate
+   file/database.*/
 var members = [{name: "Kem Crimmins", 
-				image: null,
+				image: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-1/p160x160/15871830_10209893005334994_4941021641553554237_n.jpg?oh=8ab0978065b3645e54e8409e9e2a9844&oe=5940E351",
 				description: "Kem is one of the founding members of Bronx Code Camp and the reigning wizard of JavaScript lore. Kem can often be found preaching the teachings of the YDKJS series to his followers and listening to his favorite podcast Javascript Jabber"},
 				{name: "Brian Lynch",
-				image: null,
+				image: "https://avatars1.githubusercontent.com/u/11574695?v=3&s=460",
 				description: "Brian Lynch is a freelance web designer and developer as well as a Bronx camper. In between cups of coffee, (or a craft beer depending on how late the hour is) you can find Brian working on a redesign of Tesla's website or managing the Github repository for this website."}
 				{name: "Drew Kao",
 				image: null,
