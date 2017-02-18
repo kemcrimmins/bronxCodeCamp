5343c1c14fd497df6bba9c19387166602b858901
Kem Crimmins <kem.crimmins@gmail.com>
2017-02-18 10:40:43 -0500

    seed members database in array

---
+++ displayMembers.js
@@ -0,0 +1,18 @@
+var members = [{name: "Kem Crimmins", 
+				image: null,
+				description: "Kem is one of the founding members of Bronx Code Camp and the reigning wizard of JavaScript lore. Kem can often be found preaching the teachings of the YDKJS series to his followers and listening to his favorite podcast Javascript Jabber"},
+				{name: "Brian Lynch",
+				image: null,
+				description: "Brian Lynch is a freelance web designer and developer as well as a Bronx camper. In between cups of coffee, (or a craft beer depending on how late the hour is) you can find Brian working on a redesign of Tesla's website or managing the Github repository for this website."}
+				{name: "Drew Kao",
+				image: null,
+				description: "Drew Kay is one of the founding members of Bronx Code Camp, when hes not building websites Drew can be found studying algorithms and helping his fellow campers find solutions to their coding challenges. Drew is currently managing the <em>Cracking the Coding Interview</em> study group."}
+				 ];
+
+function displayMembers() {
+
+};
+
+function highlightMembers() {
+
+};
