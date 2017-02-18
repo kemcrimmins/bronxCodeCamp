a0e29b878c18fd605f499aa15d04ccc50484cee4
Kem Crimmins <kem.crimmins@gmail.com>
2017-02-18 10:44:38 -0500

    update members database in array

--- displayMembers.js
+++ displayMembers.js
@@ -6,8 +6,15 @@ var members = [{name: "Kem Crimmins",
 				description: "Brian Lynch is a freelance web designer and developer as well as a Bronx camper. In between cups of coffee, (or a craft beer depending on how late the hour is) you can find Brian working on a redesign of Tesla's website or managing the Github repository for this website."}
 				{name: "Drew Kao",
 				image: null,
-				description: "Drew Kay is one of the founding members of Bronx Code Camp, when hes not building websites Drew can be found studying algorithms and helping his fellow campers find solutions to their coding challenges. Drew is currently managing the <em>Cracking the Coding Interview</em> study group."}
+				description: "Drew Kay is one of the founding members of Bronx Code Camp, when hes not building websites Drew can be found studying algorithms and helping his fellow campers find solutions to their coding challenges. Drew is currently managing the <em>Cracking the Coding Interview</em> study group."},
+				{name: "Rey Polanco",
+				image: null,
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget massa mollis, vestibulum eros at, efficitur lectus. Ut fermentum nisi sed turpis suscipit, quis commodo nisi tincidunt. Nunc lorem ex, maximus in enim ut, auctor cursus quam. Praesent et diam lacinia, facilisis risus vitae, tempor lacus. In pulvinar volutpat nulla vestibulum aliquam."},
+				{name: "Jose Sanchez",
+				image: null,
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget massa mollis, vestibulum eros at, efficitur lectus. Ut fermentum nisi sed turpis suscipit, quis commodo nisi tincidunt. Nunc lorem ex, maximus in enim ut, auctor cursus quam. Praesent et diam lacinia, facilisis risus vitae, tempor lacus. In pulvinar volutpat nulla vestibulum aliquam. "}
 				 ];
+				}
 
 function displayMembers() {
 
